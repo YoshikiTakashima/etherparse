082418cce9cfa3f7af1a660973910a7948efdf2b
Yoshi Takashima <ytakashl@amazon.com>
2022-06-21 20:19:42 -0400

    Added kani harness. This one fails.

--- src/lib.rs
+++ src/lib.rs
@@ -706,3 +706,26 @@ unsafe fn get_unchecked_16_byte_array(ptr: *const u8) -> [u8;16] {
         *ptr.add(15),
     ]
 }
+
+
+#[kani::unwind(11)]
+#[kani::proof]
+fn u32_u16_comparison(
+) {
+    use crate::checksum::*;
+    let kani_any_slice: [u8;0xfffusize] = kani::any();
+    let data: Vec<u8> = kani_any_slice.to_vec();
+
+    let u32_oc = u32_16bit_word::ones_complement(
+        u32_16bit_word::add_slice(0, &data)
+    );
+    let u64_oc = u64_16bit_word::ones_complement(
+        u64_16bit_word::add_slice(0, &data)
+    );
+    assert_eq!(u32_oc, u64_oc);
+
+    let struct_oc = Sum16BitWords::new()
+        .add_slice(&data)
+        .ones_complement();
+    assert_eq!(u32_oc, struct_oc);
+}
